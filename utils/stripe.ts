fa2d3dfbba9629b2c84e0070a2c30172cfba8b37
Alex <farid_qaher@yahoo.com>
2025-07-15 18:12:40 -0500

    twintyfour commit

---
+++ utils/stripe.ts
@@ -0,0 +1,9 @@
+// utils/stripe.js
+import Stripe from 'stripe';
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+  apiVersion: '2025-02-24.acacia', // Ensure you're using the correct Stripe API version.  
+
+});
+
+export default stripe;
